f2cc9332fae6d1fb08538d4dd635382164078beb
KaWaite <flippindaisy@gmail.com>
2021-06-24 17:23:12 +0900

    wip: wrapper test. Testing isn't setup correctly for tsx yet. TBD

--- test/wrapper.test.tsx
+++ test/wrapper.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Wrapper as GridWrapper } from '../stories/GridWrapper.stories';
+import GridWrapper from '../src/Grid/GridWrapper';
 
-describe('App', () => {
+describe('GridWrapper', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<GridWrapper />, div);
-    ReactDOM.unmountComponentAtNode(div);
   });
 });
