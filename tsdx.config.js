12e095aa8a55e66315d6daf43719d39f5fe7aed6
KaWaite <flippindaisy@gmail.com>
2021-06-25 15:25:03 +0900

    attempt to fix css bundling issue

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,8 +1,19 @@
+const postcss = require('rollup-plugin-postcss');
+const cssnano = require('cssnano');
 const svg = require("rollup-plugin-svg");
 
 module.exports = {
-    rollup(config) {
+    rollup(config, options) {
         config.plugins.push(
+            postcss({
+                plugins: [
+                    cssnano({
+                        preset: 'default',
+                    }),
+                ],
+                // only write out CSS for the first bundle (avoids pointless extra files):
+                extract: !!options.writeMeta,
+            }),
             svg()
         );
         return config;
