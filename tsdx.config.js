33df1bed0bbed2130446a72076d6c27e8173157d
KaWaite <flippindaisy@gmail.com>
2021-06-25 14:39:31 +0900

    remove scss related deps, remove silly test styles, remove all imports for scss related deps

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,6 +1,3 @@
-// const postcss = require('rollup-plugin-postcss');
-// const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
 const svg = require("rollup-plugin-svg");
 
 module.exports = {
