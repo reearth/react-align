c7b4b9585b2e324eb483189fde4f0085c0e944bd
KaWaite <kyle.waite@outlook.com>
2022-07-13 17:27:58 +0900

    update based on pr review

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-const postcss = require('rollup-plugin-postcss');
-const cssnano = require('cssnano');
-const svgr = require('@svgr/rollup').default;
-
-const resolvePath = (str: string) => path.resolve(__dirname, str);
+import postcss from 'rollup-plugin-postcss';
+import cssnano from 'cssnano';
+import svgr from '@svgr/rollup';
 
 export default defineConfig({
   build: {
     target: 'es2015',
     lib: {
-      entry: resolvePath('src/index.tsx'),
+      entry: 'src/index.tsx',
       name: 'ReactAlign',
       fileName: (format) => `react-align.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
     },
   },
   plugins: [
@@ -33,6 +38,6 @@ export default defineConfig({
       inject: true,
       extract: false,
     }),
-    svgr(),
+    svgr().default,
   ],
 });
