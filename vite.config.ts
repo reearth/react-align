5252a0236c4394889f345206f2ed62068c1f2588
KaWaite <kyle.waite@outlook.com>
2022-07-13 16:11:10 +0900

    fix build, gen types

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 const postcss = require('rollup-plugin-postcss');
 const cssnano = require('cssnano');
 const svgr = require('@svgr/rollup').default;
 
+const resolvePath = (str: string) => path.resolve(__dirname, str);
+
 export default defineConfig({
+  build: {
+    target: 'es2015',
+    lib: {
+      entry: resolvePath('src/index.tsx'),
+      name: 'ReactAlign',
+      fileName: (format) => `react-align.${format}.js`,
+    },
+  },
   plugins: [
+    dts({
+      rollupTypes: true,
+    }),
     postcss({
       plugins: [
         cssnano({
